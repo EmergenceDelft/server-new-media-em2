ea2d0179bc6fdb4f4dac2a338001ebc98cb02f51
Matei Cristea-Enache <mateicristeaenache88@gmail.com>
2024-05-02 15:34:00 +0200

    first websocket server connection

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/echo', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+    ws.send(`echo ${msg}`);
+  });
+  console.log('socket', req.testing);
+});
+
+
+app.listen(3000);
