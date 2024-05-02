0c6df2b8d2bdb07706b1ecdbcf8df4f61b7658b2
Matei Cristea-Enache <mateicristeaenache88@gmail.com>
2024-05-02 16:52:40 +0200

    working connection with multiple websocket clients, sending ping every second, tested it successfully with esp

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # control-new-media-em2
+
+
+when running app.js, one is able to connect via websockets to localhost:3000/echo
+the server keeps track of multiple connections (though disconnecting and reconnecting connections have not yet been tested)
+
+the server sends a message with the current timestamp to every connected cliant, every 1 second
+
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 
+var clients = [];
+
 app.use(function (req, res, next) {
   console.log('middleware');
   req.testing = 'testing';
@@ -14,12 +16,28 @@ app.get('/', function(req, res, next){
 });
 
 app.ws('/echo', function(ws, req) {
+  clients.push(ws);
+
   ws.on('message', function(msg) {
     console.log(msg);
     ws.send(`echo ${msg}`);
   });
+
+  ws.on('close', function() {
+    // Remove the WebSocket client from the array when disconnected
+    clients = clients.filter(client => client !== ws);
+  });
+
   console.log('socket', req.testing);
 });
 
+
+function sendPing() {
+  clients.forEach(client => {
+    client.send(Date.now());
+  });
+}
+
+setInterval(sendPing, 1000);
 
 app.listen(3000);
