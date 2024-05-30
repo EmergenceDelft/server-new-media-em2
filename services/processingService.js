f2a052173c2647de91374a672f1487d9f4f69070
Matei Cristea-Enache <mateicristeaenache88@gmail.com>
2024-05-30 14:26:38 +0200

    refactoring for improved readability of processingService

--- services/processingService.js
+++ services/processingService.js
@@ -3,23 +3,14 @@ import sequelize from "../services/db.js"
 import { Op } from "sequelize"
 
 const macAddressLength = 17
-let flip = true
+//let flip = true
 
 export async function processDatabaseEntries(newEntries, clients) {
-  const updatedMotors = await updateMotors(newEntries, clients)
+  const updatedMotors = await updateMotorsInDB(newEntries, clients)
 
   if (updatedMotors) {
-    const macs = updatedMotors.map((x) =>
-      x.dataValues.voxelId.slice(0, macAddressLength)
-    )
-    console.log(macs)
-
-    //now i want to send to each web socket connection the updated motors that it's interested about
-    //do that by matching ws.mac_address with updatedMotors
-
     for (let i = 0; i < clients.length; i++) {
-      const wsi = clients[i]
-      const mac_websocket = wsi.mac_address
+      const mac_websocket = clients[i].mac_address
       const filteredMotorsDegrees = updatedMotors
         .filter(
           (x) =>
@@ -29,6 +20,16 @@ export async function processDatabaseEntries(newEntries, clients) {
 
       wsi.send(motorsToJson(filteredMotorsDegrees))
     }
+  }
+}
+
+async function updateMotorsInDB(newEntries, clients) {
+  const isOverThreshold = newEntries.some((reading) => reading.value >= 0.1)
+  let macs = clients.map((client) => client.mac_address)
+  if (isOverThreshold) {
+    return dbUpdateAll(macs, 90)
+  } else {
+    return dbUpdateAll(macs, 0)
   }
 }
 
@@ -45,75 +46,35 @@ function motorsToJson(filteredMotors) {
 
   return JSON.stringify(jsonMotorFinal)
 }
+async function dbUpdateAll(macs, angle) {
+  const transaction = await sequelize.transaction()
+  try {
+    const macConditions = macs.map((mac) => ({
+      voxelId: {
+        [Op.like]: `%${mac}%`
+      }
+    }))
+    // we can no longer do this since Motors do not have module_mac_address
+    // should we do one extra query or add a redundant field into Motor creation
 
-async function updateMotors(newEntries, clients) {
-  const isOverThreshold = newEntries.some((reading) => reading.value >= 0.1)
-  console.log("threshold?")
-  console.log(isOverThreshold)
-  let macs = clients.map((client) => client.mac_address)
+    //try to extract mac from voxel_id
 
-  console.log(macs)
-
-  if (isOverThreshold) {
-    // flip = !flip
-    const transaction = await sequelize.transaction()
-    try {
-      const macConditions = macs.map((mac) => ({
-        voxelId: {
-          [Op.like]: `%${mac}%`
-        }
-      }))
-      // we can no longer do this since Motors do not have module_mac_address
-      // should we do one extra query or add a redundant field into Motor creation
-
-      //try to extract mac from voxel_id
-
-      const [numberOfAffectedRows, affectedRows] = await db.Motor.update(
-        { angle: 90 }, // Set the angle column to 90
-        {
-          where: {
-            [Op.or]: macConditions
-          },
-          returning: true,
-          transaction
-        } // sequelize interprets this as module_mac_address matches at least one value in macs
-      )
-      await transaction.commit()
-      console.log("All motors updated succesfully")
-      return affectedRows
-    } catch (error) {
-      await transaction.rollback()
-      console.error("Error updating Motors object:", error)
-    }
-  } else {
-    const transaction = await sequelize.transaction()
-    try {
-      const macConditions = macs.map((mac) => ({
-        voxelId: {
-          [Op.like]: `%${mac}%`
-        }
-      }))
-      // we can no longer do this since Motors do not have module_mac_address
-      // should we do one extra query or add a redundant field into Motor creation
-
-      //try to extract mac from voxel_id
-
-      const [numberOfAffectedRows, affectedRows] = await db.Motor.update(
-        { angle: 0 }, // Set the angle column to 90
-        {
-          where: {
-            [Op.or]: macConditions
-          },
-          returning: true,
-          transaction
-        } // sequelize interprets this as module_mac_address matches at least one value in macs
-      )
-      await transaction.commit()
-      console.log("All motors updated succesfully")
-      return affectedRows
-    } catch (error) {
-      await transaction.rollback()
-      console.error("Error updating Motors object:", error)
-    }
+    const [numberOfAffectedRows, affectedRows] = await db.Motor.update(
+      { angle: angle }, // Set the angle column to 90
+      {
+        where: {
+          [Op.or]: macConditions
+        },
+        returning: true,
+        transaction
+      } // sequelize interprets this as module_mac_address matches at least one value in macs
+    )
+    await transaction.commit()
+    console.log("All motors updated succesfully")
+    return affectedRows
+  } catch (error) {
+    await transaction.rollback()
+    console.error("Error updating Motors object:", error)
+    return null
   }
 }
