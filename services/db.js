dc6ad7980f5a348adf53b5d16806ced61b0f03cc
pjpronk <pacopronk@gmail.com>
2024-06-18 09:51:53 +0200

    Added production and development settings

--- services/db.js
+++ services/db.js
@@ -1,15 +1,27 @@
 import { Sequelize } from "sequelize"
 
-//Setup for the DB connection
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "new_media",
-  process.env.DB_USER || "nmpr",
-  process.env.DB_PASSWORD || "admin",
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres"
-  }
-)
+/* Determine environment */
+const environment = process.env.NODE_ENV || "development"
+let dbName, dbUser, dbPassword, dbHost
+
+if (environment === "production") {
+  dbName = process.env.DB_NAME || "new_media_production"
+  dbUser = process.env.DB_USER || "nmpr"
+  dbPassword = process.env.DB_PASSWORD || "admin"
+  dbHost = process.env.DB_HOST || "postgres"
+} else {
+  /* Development database configurations */
+  dbName = process.env.DB_NAME || "new_media"
+  dbUser = process.env.DB_USER || "nmpr"
+  dbPassword = process.env.DB_PASSWORD || "admin"
+  dbHost = process.env.DB_HOST || "localhost"
+}
+
+/* Setup the DB connection */
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: "postgres"
+})
 
 sequelize
   .authenticate()
